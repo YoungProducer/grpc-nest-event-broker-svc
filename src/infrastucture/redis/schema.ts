6a104c230aff8a1cc0db7774389d8522758b423a
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-05-07 21:03:34 +0300

    Added aliases for redis schemas.

--- src/infrastucture/redis/schema.ts
+++ src/infrastucture/redis/schema.ts
@@ -3,17 +3,21 @@ import { RediSearchSchema, SchemaFieldTypes } from 'redis';
 export const producerSchema: RediSearchSchema = {
   '$.name': {
     type: SchemaFieldTypes.TEXT,
+    AS: 'name',
   },
   '$.events': {
     type: SchemaFieldTypes.TEXT,
+    AS: 'events',
   },
 };
 
 export const consumerSchema: RediSearchSchema = {
   '$.name': {
     type: SchemaFieldTypes.TEXT,
+    AS: 'name',
   },
   '$.events': {
     type: SchemaFieldTypes.TEXT,
+    AS: 'events',
   },
 };
