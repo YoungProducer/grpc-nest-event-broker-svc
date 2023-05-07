e2721fb4b7a0f13bb443131c064f36b6e2b60020
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-05-07 19:45:51 +0300

    Removed `Consume` and `Produce` event methods.

--- src/domain/consumer/consumer.controller.ts
+++ src/domain/consumer/consumer.controller.ts
@@ -1,13 +1,11 @@
 import { GrpcMethod } from '@nestjs/microservices';
 import { ConsumerService } from './consumer.service';
 import { Controller } from '@nestjs/common';
-import { ServerUnaryCall, Metadata } from '@grpc/grpc-js';
 
 import { EVENT_BROKER_SERVICE_NAME } from 'src/proto/event-broker.pb';
 import { GetAllConsumersResponseDto } from './dto/get-all-consumers.response.dto';
 import { AddConsumerRequestDto } from './dto/add-consumer.request.dto';
 import { AddConsumerResponseDto } from './dto/add-consumer.response.dto';
-import { ConsumeEventRequestDto } from './dto/consume-event.request.dto';
 
 @Controller()
 export class ConsumerController {
@@ -23,14 +21,5 @@ export class ConsumerController {
   @GrpcMethod(EVENT_BROKER_SERVICE_NAME, 'getAllConsumers')
   async getAllConsumers(): Promise<GetAllConsumersResponseDto> {
     return await this.consumerService.getAllConsumers();
-  }
-
-  @GrpcMethod(EVENT_BROKER_SERVICE_NAME, 'ConsumeEvent')
-  consumeEvent(
-    dto: ConsumeEventRequestDto,
-    _metadata: Metadata,
-    call: ServerUnaryCall<any, any>,
-  ) {
-    return this.consumerService.consume(dto, call);
   }
 }
--- src/domain/producer/producer.controller.ts
+++ src/domain/producer/producer.controller.ts
@@ -5,8 +5,6 @@ import { EVENT_BROKER_SERVICE_NAME } from 'src/proto/event-broker.pb';
 import { AddProducerRequestDto } from './dto/add-producer.request.dto';
 import { AddProducerResponseDto } from './dto/add-producer.response.dto';
 import { GetAllProducersResponseDto } from './dto/get-all-producers.response.dto';
-import { ProduceEventRequestDto } from './dto/produce-event.request.dto';
-import { ProduceEventResponseDto } from './dto/produce-event.response.dto';
 
 @Controller()
 export class ProducerController {
@@ -22,12 +20,5 @@ export class ProducerController {
   @GrpcMethod(EVENT_BROKER_SERVICE_NAME, 'GetAllProducers')
   async getAllProducers(): Promise<GetAllProducersResponseDto> {
     return await this.producerService.getAllProducers();
-  }
-
-  @GrpcMethod(EVENT_BROKER_SERVICE_NAME, 'ProduceEvent')
-  async produceEvent(
-    dto: ProduceEventRequestDto,
-  ): Promise<ProduceEventResponseDto> {
-    return await this.producerService.produceEvent(dto);
   }
 }
